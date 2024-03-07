acf896f92c50fb143311132d36426d6fcf7e0851
Crowdbotics <team@crowdbotics.com>
2024-03-07 15:32:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "saba-46856",
+  url: "https://saba-46856.botics.co",
+  api: "https://saba-46856.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
